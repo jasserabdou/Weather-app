3e8cd9f13bb1f521936fb98fa5df6a41cac0f7f5
Jasser <jasserabdelfattah@12gmail.com>
2023-07-16 21:58:08 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Weather App 🌦️
+
+This is a simple Weather App built with Python, Streamlit, and the OpenWeatherMap API. It allows you to retrieve weather information for a specific city.
+
+## Setup
+
+1. Clone the repository to your local machine:
+
+```bash
+git clone https://github.com/jasserabdou/Weather-app.git
+```
+
+2. Install the required packages using pip:
+
+```bash
+pip install -r packages.txt
+```
+
+3. Obtain an API key from [OpenWeatherMap](https://home.openweathermap.org/api_keys) by signing up for an account.
+
+4. Replace `"YOUR_API_KEY"` in the code with the API key you obtained from OpenWeatherMap:
+
+```python
+api_key = "YOUR_API_KEY" 
+```
+
+## Usage
+
+1. Run the Streamlit app using the following command:
+
+```bash
+streamlit run app.py
+```
+
+2. The app will open in your default web browser.
+
+3. Enter the name of the city you want to get weather information for in the provided input field.
+
+4. Click the "Get Weather" button to retrieve and display weather data for the specified city.
+
+## Features
+
+- Displays weather information such as temperature, wind speed, clouds, humidity, pressure, timezone, and weather description for the selected city.
+- The app uses the OpenWeatherMap API to fetch weather data in real-time.
+
+## Requirements
+
+- Python 3.9 or higher
+- Streamlit
+- Requests
+- Pillow (PIL)
+
+
+
+
---
+++ app.py
@@ -0,0 +1,49 @@
+import requests
+import streamlit as st
+from PIL import Image
+
+
+def get_weather_data(city):
+    # get your api key in this link https://home.openweathermap.org/api_keys
+    api_key = "8de5dbb2d824002273709ce326a8fa33"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+def main():
+    image = Image.open("weather.jpeg")
+    st.image(image, use_column_width=True)
+    st.title("Weather App 🌦️")
+    city = st.text_input("Enter city name:")
+    if st.button("Get Weather"):
+        st.write("#### Fetching weather data...")
+        try:
+            weather_data = get_weather_data(city)
+
+            if weather_data['cod'] == 200:
+                st.header(
+                    f"Location: {weather_data['name']}, {weather_data['sys']['country']}")
+                st.subheader(f"Temperature: {weather_data['main']['temp']} °C")
+                st.subheader(
+                    f"Wind speed: {weather_data['wind']['speed']} meter/sec")
+                st.subheader(f"Clouds: {weather_data['clouds']['all']} %")
+                st.subheader(f"Humidity: {weather_data['main']['humidity']} %")
+                st.subheader(
+                    f"Pressure: {weather_data['main']['pressure']} hPa")
+                st.subheader(
+                    f"Timezone: {round(weather_data['timezone']*0.000277778,2)} hours")
+                st.subheader(
+                    f"Description: {weather_data['weather'][0]['description']}")
+            else:
+                st.error(
+                    "City not found. Please check the spelling or try a different city.")
+        except requests.exceptions.RequestException as e:
+            st.error("Error fetching weather data. Please try again later.")
+        except Exception as e:
+            st.error("An error occurred. Please try again later.")
+
+
+if __name__ == "__main__":
+    main()
